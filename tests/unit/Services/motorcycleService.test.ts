9a890275fc90ad30d47f59d4f372b4a9f6155864
Prado <peu_prado01@hotmail.com>
2023-05-17 06:12:28 -0300

    test: testa funções que fazem GET por getAll e GET por getById

--- tests/unit/Services/motorcycleService.test.ts
+++ tests/unit/Services/motorcycleService.test.ts
@@ -2,7 +2,7 @@ import * as sinon from 'sinon';
 import { Model } from 'mongoose';
 import { expect } from 'chai';
 import MotorcycleService from '../../../src/Services/MotorcyclesService';
-import { motorcycleInput, motorcycleOutput } from '../../Mocks/motorcyclesMock';
+import { motorcycleInput, motorcycleOutput, motorcycleList } from '../../Mocks/motorcyclesMock';
 
 const service = new MotorcycleService();
 describe('Testando a rota /motorcycle', function () {
@@ -12,6 +12,23 @@ describe('Testando a rota /motorcycle', function () {
       sinon.stub(Model, 'create').resolves(motorcycleOutput);
       const result = await service.createMoto(motorcycleInput);
 
+      expect(result).to.be.deep.equal(motorcycleOutput);
+    });
+  });
+  describe('Testa GET - /motorcycle -', function () {
+    it('Teste se getAll() retorna todos motorcycles ', async function () {
+      sinon.stub(Model, 'find').resolves(motorcycleList);
+      const result = await service.getAll();
+
+      expect(result).to.be.deep.equal(motorcycleList);
+    });
+  });
+
+  describe('Testa GET /motorcycle/id', function () {
+    it('Testa se retorna motorcycle correspondente ao id', async function () {
+      sinon.stub(Model, 'findById').resolves(motorcycleOutput);
+      const result = await service.getById('6455872c4c9165ea452921ba');
+  
       expect(result).to.be.deep.equal(motorcycleOutput);
     });
   });
